268bd408dfb94169f373472b9b65dd1a5b16c041
GabrielFierro237 <gabrielandro12121212@gmail.com>
2021-07-19 16:06:06 -0500

    api y trabajo grupal

---
+++ usuarios-grupal-master/src/app/agregar-usuario/agregar-usuario.component.ts
@@ -0,0 +1,35 @@
+import { UsuariosService } from './../servicios/usuarios.service';
+import { LmisUsuarios } from './../modelos/lmis-usuarios';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-agregar-usuario',
+  templateUrl: './agregar-usuario.component.html',
+  styleUrls: ['./agregar-usuario.component.scss'],
+})
+export class AgregarUsuarioComponent implements OnInit {
+  agregarUsuario: LmisUsuarios = {
+    primer_nombre: '',
+    segundo_nombre: '',
+    primer_apellido: '',
+    segundo_apellido: '',
+    rol: ''
+  };
+
+  constructor(
+    private agregar: UsuariosService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {}
+
+  guardar(hibrido: LmisUsuarios) {
+    this.agregar.agregarUsuario(hibrido).subscribe((data) => {
+      window.location.reload();
+    });
+    this.router.navigateByUrl('/users');
+  }
+}
---
+++ usuarios-grupal-master/src/app/editar-usuario/editar-usuario.component.ts
@@ -0,0 +1,53 @@
+import { ToastrService } from 'ngx-toastr';
+import { LmisUsuarios } from './../modelos/lmis-usuarios';
+import { UsuariosService } from './../servicios/usuarios.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-usuario',
+  templateUrl: './editar-usuario.component.html',
+  styleUrls: ['./editar-usuario.component.scss'],
+})
+export class EditarUsuarioComponent implements OnInit {
+  editarUsuario: LmisUsuarios = {
+    primer_nombre: '',
+    segundo_nombre: '',
+    primer_apellido: '',
+    segundo_apellido: '',
+    rol: '',
+  };
+
+  constructor(
+    private traer: ActivatedRoute,
+    private dato: UsuariosService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    const id = this.traer.snapshot.params.id;
+    console.log(id);
+
+    this.dato.obtenerUnUsuario(id).subscribe((recoger) => {
+      this.editarUsuario = recoger.body[0];
+      console.log(this.editarUsuario);
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.traer.snapshot.params.id;
+    console.log(id);
+  }
+
+  editar(id: any, hibrido: LmisUsuarios) {
+    var editarUsuario2: LmisUsuarios = {
+      primer_nombre: hibrido.primer_nombre,
+    segundo_nombre: hibrido.segundo_nombre,
+    primer_apellido: hibrido.primer_apellido,
+    segundo_apellido: hibrido.segundo_apellido,
+    rol: hibrido.rol
+    }
+    this.dato.editarUsuario(id, editarUsuario2).subscribe((data)  => {
+      window.location.reload();
+    });
+  }
+}
